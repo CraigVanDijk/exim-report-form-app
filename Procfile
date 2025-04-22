f424dfce6d94c46ac1a9a3910d4d3abdc7a863db
Craig van Dyk <CraigVD@exim.co.za>
2025-04-22 14:19:45 +0200

    Added missing app.run() to wsgi.py

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn wsgi:app  # or gunicorn main:app, depending on the file
--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,4 @@
 from app import app  # if your app is in app.py and the instance is 'app'
+
+if __name__ == "__main__":
+    app.run()
