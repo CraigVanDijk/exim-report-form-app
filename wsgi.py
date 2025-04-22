f995b431ebc23bc4be23151ef98ff91da1c3a423
Craig van Dyk <CraigVD@exim.co.za>
2025-04-22 15:10:36 +0200

    Changed requirements

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from app import app  # if your app is in app.py and the instance is 'app'
+from app import app  # Replace 'your_flask_app' with the name of your main Flask app file
 
 if __name__ == "__main__":
     app.run()
